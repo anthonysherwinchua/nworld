3dd620310729612abff54a474fe8c165bb1b9d79
anthonysherwinchua <anthonysherwinchua@gmail.com>
2017-06-04 14:27:43 +0800

    make static pages editable

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,24 @@ class PagesController < ApplicationController
   def index
   end
 
+  def about
+    @page = Page.find_by(name: 'about_us')
+    render template: 'pages/pages'
+  end
+
+  def contact
+    @page = Page.find_by(name: 'contact_us')
+    render template: 'pages/pages'
+  end
+
+  def terms
+    @page = Page.find_by(name: 'terms_and_conditions')
+    render template: 'pages/pages'
+  end
+
+  def privacy
+    @page = Page.find_by(name: 'privacy_policy')
+    render template: 'pages/pages'
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170604052831) do
+ActiveRecord::Schema.define(version: 20170604055731) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -82,6 +82,15 @@ ActiveRecord::Schema.define(version: 20170604052831) do
     t.decimal  "worth",      precision: 8, scale: 2, null: false
     t.datetime "created_at",                         null: false
     t.datetime "updated_at",                         null: false
+  end
+
+  create_table "pages", force: :cascade do |t|
+    t.string   "name"
+    t.string   "title"
+    t.string   "content"
+    t.string   "tags"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "products", force: :cascade do |t|
